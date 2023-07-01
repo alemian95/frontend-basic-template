83ac886392eea77b2897d9b0f4c1af2bf97533f9
Alessandro Mian <alessandromian95@gmail.com>
2023-07-01 18:43:56 +0200

    Settings json

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,28 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
 
+window.settings = {};
+
+let loadingSettings = fetch('./assets/settings.json')
+    .then((response) => response.json())
+    .then((json) => window.settings = json);
+
 window.addEventListener("DOMContentLoaded", (event) => {
     console.log("Document ready!");
+
+    loadingSettings
+        .then(() => {
+            console.log("Loading settings...");
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+        .finally(() => {
+            console.log("Settings ready!");
+            app();
+        });
 });
+
+function app() {
+    console.log(window.settings);
+}

c2f58a024f2885556b5c8e31a4076dba86b3fcb7
Alessandro Mian <alessandromian95@gmail.com>
2023-07-03 22:53:00 +0200

    Colors + banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,15 +25,46 @@ module.exports = {
                 secondary: {
                     DEFAULT: 'var(--color-secondary)',
                 },
+                success: {
+                    DEFAULT: 'var(--color-success)',
+                    background: 'var(--color-success-background)',
+                    text: 'var(--color-success-text)'
+                },
+                danger: {
+                    DEFAULT: 'var(--color-danger)',
+                    background: 'var(--color-danger-background)',
+                    text: 'var(--color-danger-text)'
+                },
+                warning: {
+                    DEFAULT: 'var(--color-warning)',
+                    background: 'var(--color-warning-background)',
+                    text: 'var(--color-warning-text)'
+                },
             },
             variables: {
                 DEFAULT: {
                     color: {
                         primary: {
                             DEFAULT: '#161c70',
+                            light: '#394394'
                         },
                         secondary: {
                             DEFAULT: '#c7c7c7',
+                        },
+                        success: {
+                            DEFAULT: '#2ecc71',
+                            background: '#d7f3dd',
+                            text: '#155724'
+                        },
+                        danger: {
+                            DEFAULT: '#e74c3c',
+                            background: '#f8d7da',
+                            text: '#721c24'
+                        },
+                        warning: {
+                            DEFAULT: '#f39c12',
+                            background: '#fff3cd',
+                            text: '#856404'
                         },
                     }
                 }
